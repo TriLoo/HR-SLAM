d207f93a327c57393bb3bbb1aa5c0d0d69b0dc39
smh <song_mh@yeah.net>
2017-12-24 23:30:18 +0800

    first commit

---
+++ PreFont/cmake-build-debug/CMakeFiles/SLAMPreFont.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SLAMPreFont.dir/main.cpp.o"
+  "SLAMPreFont.pdb"
+  "SLAMPreFont"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SLAMPreFont.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
