b3c3181f9cf01aaed7a110ffa434e6046ebeaddb
smh <song_mh@yeah.net>
2018-03-07 23:04:21 +0800

    finish Frame class

--- PreFont/src/frame.cpp
+++ PreFont/src/frame.cpp
@@ -5,7 +5,7 @@
 #include "myslam/frame.h"
 
 namespace myFrontEnd {
-    Frame::Frame() {
+    Frame::Frame(): id_(-1), timeStamp_(-1), camera_(nullptr) {
     }
 
     Frame::Frame(long id, double time, Sophus::SE3 Tcw, Camera::Ptr camera, cv::Mat color, cv::Mat depth) : id_(id),
@@ -19,20 +19,52 @@ namespace myFrontEnd {
 
     Frame::Ptr Frame::createFrame()
     {
-        return std::make_shared();
+        static long factory_id = 0;
+        return Frame::Ptr(new Frame(factory_id));
     }
 
     double Frame::findDepth(const cv::KeyPoint &kp)
     {
-        return depth_.at<double>(kp.pt);   // at function overloaded the Point parameter
+        int x = cvRound(kp.pt.x);    // the columns ! ! !
+        int y = cvRound(kp.pt.y);    // the Rows    ! ! !
+        ushort dep = depth_.ptr<ushort>(y)[x];
+        if (dep != 0)
+        {
+            return static_cast<double>(dep) / camera_->depth_scale_;
+        }
+        else    // return the one depth of its four nearest neighbors
+        {
+            int dx[4] = {-1, 0, 1, 0};
+            int dy[4] = {0, -1, 0, 1};
+
+            for (int i = 0; i < 4; ++i)   // rows changing
+            {
+                for (int j = 0; j < 4; ++j)       // cols changing
+                {
+                    dep = depth_.ptr<ushort>(y+dy[i])[x+dx[j]];
+                    if (dep != 0)
+                        return static_cast<double>(dep) / camera_->depth_scale_;
+                }
+            }
+        }
+
+        return -1.0;
+        //return depth_.at<double>(kp.pt);   // at function overloaded the Point parameter
     }
 
     Eigen::Vector3d Frame::getCamCenter() const
     {
-        return Eigen::Vector3d(camera_->cx_, camera_->cy_, 1);
+        return Tcw_.inverse().translation();           // TODO: why here have a inverse() ?
     }
 
     bool Frame::isInFrame(const Eigen::Vector3d &pt_world)
     {
+        Eigen::Vector3d p_c = camera_->world2camera(pt_world, Tcw_);
+        if (p_c(3, 0) < 0)
+            return false;
+
+        Eigen::Vector2d pixel = camera_->camera2pixel(p_c);
+        return pixel(0, 0) > 0 && pixel(0, 0) < color_.cols          // pixel(0, 0): cols ! ! !
+                && pixel(1, 0) > 0 && pixel(1, 0) < color_.rows;     // pixel(1, 0): rows ! ! !
     }
 }
