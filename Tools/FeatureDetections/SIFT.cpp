f29c78378244c603a055598d2930ad1d224b4a5f
smh <song_mh@yeah.net>
2018-05-17 08:04:36 +0800

    update

--- Tools/FeatureDetections/SIFT.cpp
+++ Tools/FeatureDetections/SIFT.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 using namespace cv;
 
+#define ATF at<float>
+
 namespace Feature
 {
     FeatureSIFT::FeatureSIFT(int o, double a, double ct, double cu_t): octaves_(o), alphas_(a), contrast_threshold_(ct), curvature_threshold_(cu_t)
@@ -17,13 +19,16 @@ namespace Feature
 
     void FeatureSIFT::initSIFT(const cv::Mat& img)
     {
+        // TODO: Assure that the input is grayscale image
         Mat imgIn, imgTemp;
+        int row = img.rows, col = img.cols;
 
+        // Create different scales
         int k = pow(2, 1.0/scales_);
         Size gaussSize(5, 5);                  // size of gaussian filter: 5 * 5
         for (int i = 0; i < octaves_; ++i)
         {
-            for (int j = 0; j < scales_; ++j)
+            for (int j = 0; j < scales_ + 3; ++j)   // 每一个Octave生成Scale+3个不同尺度图像
             {
                 alpha = pow(2, i - 1) * pow(k, j) * alphas_;
 
@@ -33,12 +38,122 @@ namespace Feature
             }
 
             // Subsample the image
-            resize(imgIn, imgIn, Size(), 0.5, 0.5, INTER_AREA);
+            //resize(imgIn, imgIn, Size(), 0.5, 0.5, INTER_AREA);
+            pyrDown(imgIn, imgIn);
+            PyrGauss_.push_back(imgIn);
+        }
+
+        // Create DoG using above results
+        for (int i = 0; i < octaves_; ++i)
+            for (int j = 0; j < scales_ + 2; ++j)        // 每一层Octave生层Scale+2个DoG
+            {
+                int octShift = i * scales_;
+                imgTemp = PyrGauss_[octShift + j + 1] -  PyrGauss_[octShift + j];
+                PyrDoGs_.push_back(imgTemp);
+            }
+
+        // Create extremum index mat
+        int index = 0;
+        for (int i = 0; i < octaves_; ++i)
+        {
+            for (int j = 0; j < scales_; ++j)   // 每一层Octave去掉两个DoG，剩下Scale个极值Flag图
+            {
+                extremums_.push_back(Mat::zeros(PyrDoGs_[index].size(), CV_32FC1));
+            }
+            index = octaves_ * (scales_ + 2);
+        }
+
+        // Extract local extremum points
+        extractExtremum();
+
+        // Filter out bad points
+
+        // Assign Orientations
+
+    }
+
+    void FeatureSIFT::extractExtremum()
+    {
+        // To extract the minimum & maximum locations
+        int indexA = 0, indexB = 0;
+        Mat temp;
+        vector<float> NeighborPixels;
+        float currPixel;
+        int MaxY = 0, MinY = 0;
+        for (int i = 0; i < octaves_; ++i)
+        {
+            // 待处理的数据是Scale_ + 2个DoG图像，输出时Scale个极值信息图
+            for (int j = 1; j < scales_ + 1; ++j)   // 每一层octave，生成Scale_个具有最大值信息的图像
+            {
+                indexA = i * (scales_ + 2) + j;   // Read
+                indexB = i * scales_ + j - 1;     // Write
+                temp = PyrDoGs_[indexA];
+                for (int m = 1; m < temp.rows - 1; ++m)
+                {
+                    for (int n = 1; n < temp.cols - 1; ++n)
+                    {
+                        currPixel = temp.ATF(m, n);
+                        // current layer
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i-1, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i-1, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i-1, j+1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i, j+1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i+1, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i+1, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA].ATF(i+1, j+1));
+                        // above layer
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i-1, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i-1, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i-1, j+1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i, j+1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i+1, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i+1, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA - 1].ATF(i+1, j+1));
+                        // below layer
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i-1, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i-1, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i-1, j+1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i, j+1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i+1, j-1));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i+1, j));
+                        NeighborPixels.push_back(PyrDoGs_[indexA + 1].ATF(i+1, j+1));
+
+                        //assert(NeighborPixels.size() >= 26);
+                        // OR
+                        assert(!NeighborPixels.empty());
+
+                        // Determine the maximum
+                        sort(NeighborPixels.begin(), NeighborPixels.end());  // in ascending order
+                        // if the vector is empty, generate undefined behavior, Similarly Hereinafter
+                        if (currPiexl > *NeighborPixels.back())   // const
+                            MaxY  = 1;
+                        else
+                            MaxY = 0;
+
+                        if (currPixel < NeighborPixels.front())
+                            MinY = 1;
+                        else
+                            MinY = 0;
+
+                        // wirte the result back to extremums_
+                        extremums_[indexB].ATF(i, j) = MinY | MaxY;
+
+                        // clear the vector
+                        NeighborPixels.clear();
+                    }
+                }
+            }
         }
     }
 
     void FeatureSIFT::calcSIFT(cv::Mat &imgOut, const cv::Mat &imgIn)
     {
+        //
     }
 }
 
--- Tools/FeatureDetections/SIFT.h
+++ Tools/FeatureDetections/SIFT.h
@@ -12,13 +12,13 @@ namespace Feature
     class FeatureSIFT
     {
     public:
-        FeatureSIFT(int o = 3, double a = 0.5, double ct = 0.03, double cu_t = 10.0);
+        FeatureSIFT(int o = 3, int s = 3, double a = 0.5, double ct = 0.03, double cu_t = 10.0);
         ~FeatureSIFT();
 
         void calcSIFT(cv::Mat& imgOut, const cv::Mat& imgIn);
     private:
-        int octaves_;          // levels of different resolution(图像的分辨率不同), 即Octave == (S)
-        int scales_;           // number of different scales (alpha in Gaussian Filter)
+        int octaves_;          // levels of different resolution(图像的分辨率不同)
+        int scales_;           // number of different scales (alpha in Gaussian Filter), 即Octave == (S)
         double alphas_;         // numbers of different scale(高斯滤波的方差大小不同),此外，k = 2^{1/s}
         double contrast_threshold_, curvature_threshold_;      // 阈值：对比度阈值，默认为0.03；曲率阈值，默认为10.
 
@@ -31,12 +31,14 @@ namespace Feature
         std::vector<int> kpScales_;                            // 特征点所处的尺度
         std::vector<double> kpGradOrients_;                    // 特征点的梯度方向
         std::vector<double> kpGradMags_;                       // 特征点梯度幅值
+        std::vector<cv::Mat> extremums_;
 
         // 保存描述子: 每一个特征点对应一个128维的向量
         cv::Mat kpDescrips_;
 
         // Some Operations to Complete 'calcSIFT(...)'
         void initSIFT(const cv::Mat& img);
+        void extractExtremum();
     };
 }
 
