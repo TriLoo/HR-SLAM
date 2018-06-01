aaf22dc2cd50979f22d8636b6c6b7d8c965556bb
smh <song_mh@yeah.net>
2018-06-01 23:35:32 +0800

    update

---
+++ LearningApproaches/YOLOv2/model.py
@@ -0,0 +1,86 @@
+import mxnet as mx
+from mxnet import gluon
+from mxnet import autograd
+from mxnet import nd
+
+def conv_at_layer(in_data, kernel, stride, num_filter, activation='leaky', padding=1, use_bn = True):
+    out_data = mx.symbol.Convolution(in_data, kernel=kernel, stride=stride, pad=padding, num_filter=num_filter)
+    # use BN before the activation
+    if use_bn:
+        out_data = mx.symbol.BatchNorm(out_data)
+    out_data = mx.symbol.LeakyReLU(out_data, act_type=activation)
+
+    return out_data
+
+
+# Construct the blocks using Function
+def conv3_4block(output_channel):
+    mid_channel = output_channel // 2
+    blk = gluon.nn.HybridSequential()
+    blk.add(
+        gluon.nn.Conv2D(output_channel, 3, padding=1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.Conv2D(mid_channel, 1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.Conv2D(output_channel, 3, padding=1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.MaxPool2D(strides=2)
+    )
+
+    return blk
+
+
+def conv5_6block(output_channel):
+    mid_channel = output_channel // 2
+    blk = gluon.nn.HybridSequential()
+    blk.add(
+        gluon.nn.Conv2D(output_channel, 3, padding=1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.Conv2D(mid_channel, 1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.Conv2D(output_channel, 3, padding=1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.Conv2D(mid_channel, 1),
+        gluon.nn.LeakyReLU(0.1),
+        gluon.nn.Conv2D(output_channel, 3, padding=1),
+        gluon.nn.LeakyReLU(0.1),
+    )
+
+    return blk
+
+
+
+
+class DarkNet19(gluon.nn.HybridBlock):
+    def __init__(self, num_class, **kwargs):
+        super(DarkNet19, self).__init__(**kwargs)
+        self.net = gluon.nn.HybridSequential()
+        with self.net.name_scope():
+            self.net.add(
+                gluon.nn.Conv2D(32, 3, padding=1),
+                gluon.nn.LeakyReLU(0.1),                # conv1, Set the alpha to 0.1
+                gluon.nn.MaxPool2D(strides=2),          # pool1
+                gluon.nn.Conv2D(64, 3, padding=1),
+                gluon.nn.LeakyReLU(0.1),                # conv2 end
+                gluon.nn.MaxPool2D(strides=2),          # pool2
+                conv3_4block(128),                      # conv3 + pool3
+                conv3_4block(256),                      # conv4 + pool4
+                conv5_6block(512),                      # conv5
+                gluon.nn.MaxPool2D(strides=2),          # pool5
+                conv5_6block(1024)
+            )
+
+        self.conv7 = gluon.nn.Conv2D(1000, kernel_size=1)
+        self.avgpool = gluon.nn.GlobalAvgPool2D()
+
+
+    def hybrid_forward(self, F, x, *args, **kwargs):
+        mid_x = self.net(x)
+        mid_x = self.conv7(mid_x)
+        y = self.avgpool(mid_x)
+
+        # OR
+        #return mx.symbol.softmax(y)
+        return nd.Softmax(y)
+
+
