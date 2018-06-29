446911e6453851dc4fd041801f4178f875f39084
smh <song_mh@yeah.net>
2018-06-29 23:29:49 +0800

    update

--- LearningApproaches/InstanceSegmentationRGBD/model.py
+++ LearningApproaches/InstanceSegmentationRGBD/model.py
@@ -8,12 +8,70 @@ def stnlayer(data):
     return mx.sym.SpatialTransformer(data, transform_type='affine')
 
 
+def get_resblock_downsample(output_num):
+    mid_output_num = output_num >> 1
+    net = gluon.nn.HybridSequential()
+    net.add(
+        gluon.nn.Conv2D(output_num, 3, 1, 1),
+        gluon.nn.Conv2D(mid_output_num, 1, 1),
+        gluon.nn.Conv2D(output_num, 3, 1, 1)
+    )
+
+    return net
+
+
+# Orange Block: ResLayer with Downsample
+class resblock_downsample(gluon.nn.HybridBlock):
+    def __init__(self, output_nums, **kwargs):
+        super(resblock_downsample, self).__init__(**kwargs)
+
+        mid_output_nums = output_nums >> 1
+
+        self.conv1 = gluon.nn.Conv2D(output_nums, kernel_size=3, strides=1, padding=1)
+        self.bn1 = gluon.nn.BatchNorm()
+        self.actv1 = gluon.nn.Activation('relu')
+        self.conv2 = gluon.nn.Conv2D(mid_output_nums, kernel_size=1, strides=1)
+        self.bn2 = gluon.nn.BatchNorm()
+        self.actv2 = gluon.nn.Activation('relu')
+        self.conv3 = gluon.nn.Conv2D(output_nums, kernel_size=3, strides=1, padding=1)
+        self.bn3 = gluon.nn.BatchNorm()
+        self.actv3 = gluon.nn.Activation('relu')
+
+    def hybrid_forward(self, F, x, *args, **kwargs):
+        conv = self.actv1(self.bn1(self.conv1(x)))
+        conv = self.actv2(self.bn2(self.conv1(x)))
+        conv = self.actv3(self.bn3(self.conv1(x)))
+
+        return F.relu(conv + x)
+
+
+# Pink Block: ResLayer with Upsample
+class resblock_upsample(gluon.nn.SymbolBlock):
+    def __init__(self, num_outputs, **kwargs):
+        super(resblock_upsample, self).__init__(**kwargs)
+
+        mid_output_num = num_outputs >> 1
+
+        self.conv1 = gluon.nn.Conv2D(num_outputs, kernel_size=3, strides=1, padding=1)
+        self.bn1 = gluon.nn.BatchNorm()
+        self.upconv1 = gluon.nn.Conv2DTranspose(mid_output_num, kernel_size=4, strides=2, padding=1, use_bias=False, weight_initializer=mx.init.Bilinear())
+        self.upconv1_side = gluon.nn.Conv2DTranspose(mid_output_num, kernel_size=4, strides=2, padding=1, use_bias=False, weight_initializer=mx.init.Bilinear())
+
+    def hybrid_forward(self, F, x, *args, **kwargs):
+        upconv = self.upconv1(F.relu(self.bn1(self.conv1(x))))
+        upconv_side = self.upconv1_side(x)
+
+        return F.relu(upconv + upconv_side)
+
 
 class ASENet(gluon.nn.HybridBlock):
     def __init__(self, **kwargs):
         super(ASENet, self).__init__(**kwargs)
 
-    def hybrid_forward(self, F, x, *args, **kwargs):
+        self.conv1 = gluon.nn.Conv2D(32, kernel_size=3, strides=1, padding=1)
+        self.conv1_d = gluon.nn.Conv2D(32, kernel_size=3, strides=1, padding=1)
+
+    def hybrid_forward(self, F, rgb, depth, *args, **kwargs):
         pass
 
 
--- LearningApproaches/InstanceSegmentationRGBD/readme.md
+++ LearningApproaches/InstanceSegmentationRGBD/readme.md
@@ -18,3 +18,13 @@ This folder includes the implementation of my own network to realize instance-le
 [3] Mask R-CNN
 
 ...
+
+## Logs
+
+2018.06.28
+
+决定先基于<RedNet: Residual Encoder-Decoder Network for indoor RGB-D Semantic Segmentation>
+
+开发基础网络结构。现在决定的是，引入STN, 修改目标函数，引入Instance-level segmentation(还不确定怎么做)。
+
+基础网络基于ResNet-50来实现, 后续考虑添加旁路。
