0dec7629e56b59e3a5e7ce6cc0ea7aff54869a9e
smh <song_mh@yeah.net>
2018-07-02 23:40:33 +0800

    updating

--- LearningApproaches/InstanceSegmentationRGBD/model.py
+++ LearningApproaches/InstanceSegmentationRGBD/model.py
@@ -1,67 +1,74 @@
 import mxnet as mx
 from mxnet import gluon
-from mxnet import nd
 
+# Cautions:  backbone network: ResNet-50 (RedNet)
 
 def stnlayer(data):
     #net = gluon.nn.HybridSequential()
     return mx.sym.SpatialTransformer(data, transform_type='affine')
 
 
-def get_resblock_downsample(output_num):
-    mid_output_num = output_num >> 1
+def get_reslayer_downsample(output_num, ks, s, p=None, act=False):
     net = gluon.nn.HybridSequential()
     net.add(
-        gluon.nn.Conv2D(output_num, 3, 1, 1),
-        gluon.nn.Conv2D(mid_output_num, 1, 1),
-        gluon.nn.Conv2D(output_num, 3, 1, 1)
+        gluon.nn.Conv2D(output_num, kernel_size=ks, strides=s, padding=p),
+        gluon.nn.BatchNorm()
     )
+
+    if act:
+        net.add(gluon.nn.Activation('relu'))
+
+    return net
+
+
+def get_reslayer_upsample(output_num, ks=4, s=2, p=1, act=False):
+    net = gluon.nn.HybridSequential()
+    net.add(
+        gluon.nn.Conv2DTranspose(output_num, kernel_size=ks, strides=s, padding=p),
+        gluon.nn.BatchNorm()
+    )
+
+    if act:
+        net.add(gluon.nn.Activation('relu'))
 
     return net
 
 
 # Orange Block: ResLayer with Downsample
+# the pattern of resnet-50
 class resblock_downsample(gluon.nn.HybridBlock):
     def __init__(self, output_nums, **kwargs):
         super(resblock_downsample, self).__init__(**kwargs)
 
-        mid_output_nums = output_nums >> 1
-
-        self.conv1 = gluon.nn.Conv2D(output_nums, kernel_size=3, strides=1, padding=1)
-        self.bn1 = gluon.nn.BatchNorm()
-        self.actv1 = gluon.nn.Activation('relu')
-        self.conv2 = gluon.nn.Conv2D(mid_output_nums, kernel_size=1, strides=1)
-        self.bn2 = gluon.nn.BatchNorm()
-        self.actv2 = gluon.nn.Activation('relu')
-        self.conv3 = gluon.nn.Conv2D(output_nums, kernel_size=3, strides=1, padding=1)
-        self.bn3 = gluon.nn.BatchNorm()
-        self.actv3 = gluon.nn.Activation('relu')
+        mid_output_nums = output_nums >> 1    # output channel numbers of the first conv layer
+        lst_output_nums = output_nums << 1    # output channel numbers of the last conv layer
+        self.conv1 = get_reslayer_downsample(mid_output_nums, ks=1, s=1, act=True)
+        self.conv2 = get_reslayer_downsample(output_nums, ks=3, s=2, p=1, act=True)         # the stride = 2, dimension halfed
+        self.conv3 = get_reslayer_downsample(lst_output_nums, ks=1, s=1, act=False)
+        self.conv1_side = get_reslayer_downsample(lst_output_nums, ks=1, s=2, act=False)
 
     def hybrid_forward(self, F, x, *args, **kwargs):
-        conv = self.actv1(self.bn1(self.conv1(x)))
-        conv = self.actv2(self.bn2(self.conv1(x)))
-        conv = self.actv3(self.bn3(self.conv1(x)))
-
-        return F.relu(conv + x)
+        conv1 = self.conv3(self.conv2(self.conv1(x)))
+        conv1_side = self.conv1_side(x)
+        return F.relu(conv1 + conv1_side)
 
 
 # Pink Block: ResLayer with Upsample
 class resblock_upsample(gluon.nn.SymbolBlock):
-    def __init__(self, num_outputs, **kwargs):
+    def __init__(self, output_nums, **kwargs):
         super(resblock_upsample, self).__init__(**kwargs)
 
-        mid_output_num = num_outputs >> 1
+        lst_output_nums = output_nums >> 1     # the output channels is halfed
 
-        self.conv1 = gluon.nn.Conv2D(num_outputs, kernel_size=3, strides=1, padding=1)
-        self.bn1 = gluon.nn.BatchNorm()
-        self.upconv1 = gluon.nn.Conv2DTranspose(mid_output_num, kernel_size=4, strides=2, padding=1, use_bias=False, weight_initializer=mx.init.Bilinear())
-        self.upconv1_side = gluon.nn.Conv2DTranspose(mid_output_num, kernel_size=4, strides=2, padding=1, use_bias=False, weight_initializer=mx.init.Bilinear())
+        self.conv1 = get_reslayer_downsample(output_nums, ks=3, s=1, act=True)
+        self.deconv1 = get_reslayer_upsample(lst_output_nums)               # upsample the dimension & half the channels
+        self.deconv1_side = get_reslayer_upsample(lst_output_nums)
 
     def hybrid_forward(self, F, x, *args, **kwargs):
-        upconv = self.upconv1(F.relu(self.bn1(self.conv1(x))))
-        upconv_side = self.upconv1_side(x)
+        upconv1 = self.deconv1(self.conv1(x))
+        upconv1_side = self.deconv1_side(x)
 
-        return F.relu(upconv + upconv_side)
+        return F.relu(upconv1 + upconv1_side)
 
 
 class ASENet(gluon.nn.HybridBlock):
--- LearningApproaches/InstanceSegmentationRGBD/readme.md
+++ LearningApproaches/InstanceSegmentationRGBD/readme.md
@@ -28,3 +28,18 @@ This folder includes the implementation of my own network to realize instance-le
 开发基础网络结构。现在决定的是，引入STN, 修改目标函数，引入Instance-level segmentation(还不确定怎么做)。
 
 基础网络基于ResNet-50来实现, 后续考虑添加旁路。
+
+2018.07.02
+
+这两天看了几篇论文。主要分为early fusion, late fusion, middle fusion之类的，但貌似Multi-scale Fusion
+
+还是最好的，所以网络结构应该具有多尺度feature fusion的特点；其次，加入我的想法，就是引入Attention
+
+机制。
+
+现在的损失函数都是基于Cross Entropy来做的，这样肯定是不能实现Instance-level Segmentation。
+
+中间<Learning common and specific features ...> 这篇文章的目标函数比较有新意。
+
+下一步怎么做？
+
