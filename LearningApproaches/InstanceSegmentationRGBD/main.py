f22f2593a98aa3c3b39df717dc127619d602992c
smh <song_mh@yeah.net>
2018-07-12 21:10:57 +0800

    fixed bugs

--- LearningApproaches/InstanceSegmentationRGBD/main.py
+++ LearningApproaches/InstanceSegmentationRGBD/main.py
@@ -1,5 +1,117 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'smh'
+__date__ = '2018.07.12'
+
+import mxnet as mx
 import model
 from mxnet import nd
+import argparse
 
+'''
 x = nd.random_uniform(0, 1, shape=(1, 3, 320, 1024))
+
+net = model.resblock_same(256)
+net.initialize(mx.init.Xavier())
+net.hybridize()
+rgb = nd.random_uniform(0, 1, shape=(1, 256, 512, 512))
+y = net(rgb)
+print(y.shape)
+'''
+
+'''
+# Test the model
+net = model.ASENet(10)
+net.initialize(init=mx.init.Xavier())
+net.hybridize()
+#print(net)
+
+rgb = nd.random_uniform(0, 1, shape=(1, 3, 512, 512))
+depth = nd.random_uniform(0, 1, shape=(1, 1, 512, 512))
+
+a, b, c, d, e = net(rgb, depth)
+print('a.shape = ', a.shape)
+print('b.shape = ', a.shape)
+print('c.shape = ', a.shape)
+print('d.shape = ', a.shape)
+print('e.shape = ', a.shape)
+'''
+'''
+net = model.resblock_downsample(256)
+net.initialize(mx.init.Xavier())
+net.hybridize()
+
+x = nd.random_uniform(0, 1, (1, 64, 512, 512))
+
+y = net(x)
+
+print(y.shape)
+'''
+
+'''
+net = model.resblock_upsample(256)
+net.initialize(mx.init.Xavier())
+net.hybridize()
+
+x = nd.random_uniform(0, 1, (1, 512, 128, 128))
+y = net(x)
+print(y.shape)
+'''
+
+'''
+net = model.get_resblock_encoder(512, 4, True)
+net.initialize(mx.init.Xavier())
+net.hybridize()
+
+x = nd.random_uniform(0, 1, (1, 256, 128, 128))
+y = net(x)
+print(y.shape)
+'''
+
+'''
+net = model.get_resblock_encoder(256, 3, False)
+net.initialize(mx.init.Xavier())
+net.hybridize()
+
+x = nd.random_uniform(0, 1, (1, 64, 128, 128))
+y = net(x)
+print(y.shape)
+'''
+
+'''
+#net = model.get_resblock_decoder(256, 6, True)
+net = model.get_resblock_decoder(256, 6, False)
+net.initialize(mx.init.Xavier())
+net.hybridize()
+
+x = nd.random_uniform(0, 1, (1, 512, 64, 64))
+y = net(x)
+print(y.shape)
+'''
+
+def main():
+    parse = argparse.ArgumentParser()
+    parse.add_argument('--epoches', type=int, default=10)
+    args = parse.parse_args()
+    net = model.ASENet(10)
+    net.initialize(mx.init.Xavier())
+    net.hybridize()
+    #print(net)
+
+    rgb = nd.random_uniform(0, 1, (1, 3, 512, 512))
+    depth = nd.random_uniform(0, 1, (1, 1, 512, 512))
+
+    y1, y2, y3, y4, y5 = net(rgb, depth)
+    #y1, y2 = net(rgb, depth)
+
+    print('y1.shape: ', y1.shape)
+    print('y2.shape: ', y2.shape)
+    print('y3.shape: ', y3.shape)
+    print('y4.shape: ', y4.shape)
+    print('y5.shape: ', y5.shape)
+
+
+if __name__ == '__main__':
+    main()
+
 
--- LearningApproaches/InstanceSegmentationRGBD/model.py
+++ LearningApproaches/InstanceSegmentationRGBD/model.py
@@ -11,12 +11,13 @@ from mxnet import gluon
 
 # 文档结构： (1). RedNet Model, (2). Loss class, (3). Training function, etc.
 
+
 def stnlayer(data):
     #net = gluon.nn.HybridSequential()
     return mx.sym.SpatialTransformer(data, transform_type='affine')
 
 
-def get_convlayer(output_num, ks, s, p=None, bn=True, act=False):
+def get_convlayer(output_num, ks, s, p=0, bn=True, act=False):
     net = gluon.nn.HybridSequential()
     net.add(
         gluon.nn.Conv2D(output_num, kernel_size=ks, strides=s, padding=p, use_bias=False),
@@ -43,14 +44,32 @@ def get_reslayer_upsample(output_num, ks=4, s=2, p=1, act=False):
     return net
 
 
-# Green BlocK: residual layer with no downsample
-# 即普通的具有bottleneck结构的res block
+# Green Block: residual layer with no downsample
 class resblock_same(gluon.nn.HybridBlock):
     def __init__(self, output_nums, **kwargs):
-        super(resblock_same, self).__init__(**kwargs)
+        super(resblock_same, self).__init__()
 
         mid_output_nums = output_nums >> 2     # the first two layer in a bottleneck of resdual layer is 1/4 input channels
-        self.conv1 = get_convlayer(mid_output_nums, ks=1, s=1, p=None, act=True)
+        self.conv1 = get_convlayer(mid_output_nums, ks=3, s=1, p=1, act=True)
+        self.conv2 = get_convlayer(mid_output_nums, ks=3, s=1, p=1, act=True)
+        self.conv3 = get_convlayer(output_nums, ks=3, s=1, p=1, act=False)
+        self.conv1_side = get_convlayer(output_nums, ks=1, s=1, p=0, act=False)
+
+    def hybrid_forward(self, F, x, *args, **kwargs):
+        conv1 = self.conv3(self.conv2(self.conv1(x)))
+        # 无论是ResNet还是Identity ResNet都是通过一个旁路来保证channel的一致，Identity ResNet文章说，
+        # 这样的旁路不多，可以忽略，或者文章中的推导可以认为是适用于相同size的feature map而言的。
+        conv1_side = self.conv1_side(x)
+        return F.relu(conv1_side + conv1)
+
+
+# 即普通的具有bottleneck结构的res block
+class bottleneck(gluon.nn.HybridBlock):
+    def __init__(self, output_nums, **kwargs):
+        super(bottleneck, self).__init__(**kwargs)
+
+        mid_output_nums = output_nums >> 2     # the first two layer in a bottleneck of resdual layer is 1/4 input channels
+        self.conv1 = get_convlayer(mid_output_nums, ks=1, s=1, p=0, act=True)
         self.conv2 = get_convlayer(mid_output_nums, ks=3, s=1, p=1, act=True)
         self.conv3 = get_convlayer(output_nums, ks=1, s=1, act=False)
 
@@ -66,12 +85,12 @@ class resblock_downsample(gluon.nn.HybridBlock):
     def __init__(self, output_nums, **kwargs):
         super(resblock_downsample, self).__init__(**kwargs)
 
-        mid_output_nums = output_nums >> 1    # output channel numbers of the first conv layer
-        lst_output_nums = output_nums << 1    # output channel numbers of the last conv layer
+        mid_output_nums = output_nums >> 2    # output channel numbers of the first conv layer
+        #lst_output_nums = output_nums    # output channel numbers of the last conv layer
         self.conv1 = get_convlayer(mid_output_nums, ks=1, s=1, act=True)
-        self.conv2 = get_convlayer(output_nums, ks=3, s=2, p=1, act=True)         # the stride = 2, dimension halfed
-        self.conv3 = get_convlayer(lst_output_nums, ks=1, s=1, act=False)
-        self.conv1_side = get_convlayer(lst_output_nums, ks=1, s=2, act=False)
+        self.conv2 = get_convlayer(mid_output_nums, ks=3, s=2, p=1, act=True)         # the stride = 2, dimension halfed
+        self.conv3 = get_convlayer(output_nums, ks=1, s=1, act=False)
+        self.conv1_side = get_convlayer(output_nums, ks=1, s=2, act=False)
 
     def hybrid_forward(self, F, x, *args, **kwargs):
         conv1 = self.conv3(self.conv2(self.conv1(x)))
@@ -80,15 +99,15 @@ class resblock_downsample(gluon.nn.HybridBlock):
 
 
 # Pink Block: ResLayer with Upsample
-class resblock_upsample(gluon.nn.SymbolBlock):
+class resblock_upsample(gluon.nn.HybridBlock):
     def __init__(self, output_nums, **kwargs):
         super(resblock_upsample, self).__init__(**kwargs)
 
-        lst_output_nums = output_nums >> 1     # the output channels is halfed
+        mid_output_nums = output_nums << 1     # the output channels is halfed
 
-        self.conv1 = get_convlayer(output_nums, ks=3, s=1, act=True)
-        self.deconv1 = get_reslayer_upsample(lst_output_nums)               # upsample the dimension & half the channels
-        self.deconv1_side = get_reslayer_upsample(lst_output_nums)
+        self.conv1 = get_convlayer(mid_output_nums, ks=3, s=1, p=1, act=True)
+        self.deconv1 = get_reslayer_upsample(output_nums)               # upsample the dimension & half the channels
+        self.deconv1_side = get_reslayer_upsample(output_nums)
 
     def hybrid_forward(self, F, x, *args, **kwargs):
         upconv1 = self.deconv1(self.conv1(x))
@@ -105,32 +124,43 @@ def get_resblock_encoder(output_nums, unit_nums, down=True):
     if down:       # encoder中的除了第一层layer之外的layer的结构，先进行降维，然后后面跟着几个具有bottleneck的res block
         blk.add(resblock_downsample(output_nums))       # 利用res layer with downsample首先进行降维
         for i in range(unit_nums - 1):
-            blk.add(resblock_same(output_nums))
+            blk.add(bottleneck(output_nums))
     else:                                               # 这里主要用于实现encoder里面的第一层layer，即不降维
-        for i in range(unit_nums):                      # 不降维的话，就是普通的绿色block, 即全是具有bottleneck的res block
-            blk.add(resblock_same(output_nums))
-
+        blk.add(resblock_same(output_nums))                 # 但是block之间存在channel的翻倍关系，通过旁落来保证channel一致
+        for i in range(unit_nums - 1):                      # 不降维的话，就是普通的绿色block, 即全是具有bottleneck的res block
+            blk.add(bottleneck(output_nums))
     return blk
 
 
 # 文章说了，在decoder部分的trans1-5里面，每层trans只有一层是具有升维功能的res layer with upsample， 其它层是具有普通两层3*3卷积同等输出channel的res block.
 # 在decoder的最后一层是一层Conv2DTranspose实现，而不是包含res layer with upsample的trans层实现的
 # 注意，与encoder部分不同的是，在每一个trans结构里面，是先存在几个不降维的res block (unit)，在最后才会跟一层 res layer with upsample
-def get_resblock_decoder(output_nums, unit_nums, up=True):
+def get_resblock_decoder(output_nums, unit_nums, dimskeep=False, up=True):
     blk = gluon.nn.HybridSequential()
     if up:
-        for i in range(unit_nums-1):
-            blk.add(
-                resblock_same(output_nums)     # 这里与原文不同，原文这里并没有采用bottleneck结构，而是普通的两层3*3同等输出channel数量的卷积层，见ResNet论文,为了方便，这里采用bottleneck 结构
-            )
-        blk.add(resblock_upsample(output_nums))
+        if dimskeep:
+            for i in range(unit_nums-1):
+                blk.add(bottleneck(output_nums))
+            blk.add(resblock_upsample(output_nums))
+        else:
+            for i in range(unit_nums-1):
+                blk.add(
+                    bottleneck(output_nums << 1)     # 这里与原文不同，原文这里并没有采用bottleneck结构，而是普通的两层3*3同等输出channel数量的卷积层，见ResNet论文,为了方便，这里采用bottleneck 结构
+                )
+            blk.add(resblock_upsample(output_nums))
     else:                                       # else分支作为实现Trans5的基础，因为Trans5并没有升维，而是配合后续的一个Transpose Conv layer来实现升维！
-        for i in range(unit_nums):
-            blk.add(
-                resblock_same(output_nums)     # 这里与原文不同，原文这里并没有采用bottleneck结构，而是普通的两层3*3同等输出channel数量的卷积层，见ResNet论文,为了方便，这里采用bottleneck 结构
-            )
-
-
+        if dimskeep:
+            for i in range(unit_nums - 1):
+                blk.add(
+                    bottleneck(output_nums)     # 这里与原文不同，原文这里并没有采用bottleneck结构，而是普通的两层3*3同等输出channel数量的卷积层，见ResNet论文,为了方便，这里采用bottleneck 结构
+                )
+            blk.add(resblock_same(output_nums))
+        else:
+            for i in range(unit_nums - 1):
+                blk.add(
+                    bottleneck(output_nums << 1)     # 这里与原文不同，原文这里并没有采用bottleneck结构，而是普通的两层3*3同等输出channel数量的卷积层，见ResNet论文,为了方便，这里采用bottleneck 结构
+                )
+            blk.add(resblock_same(output_nums))
     return blk
 
 
@@ -150,8 +180,8 @@ class ASENet(gluon.nn.HybridBlock):
         self.conv1 = get_convlayer(64, ks=7, s=2, p=3, act=True)
         self.conv1_d = get_convlayer(64, ks=7, s=2, p=3, act=True)
         # max pooling layer 1 with stride = 2
-        self.maxpool1 = gluon.nn.MaxPool2D(pool_size=3, strides=2)
-        self.maxpool1_d = gluon.nn.MaxPool2D(pool_size=3, strides=2)
+        self.maxpool1 = gluon.nn.MaxPool2D(pool_size=3, strides=2, padding=1)
+        self.maxpool1_d = gluon.nn.MaxPool2D(pool_size=3, strides=2, padding=1)
         # block layer 1 of encoder part
         self.encoder_layer1 = get_resblock_encoder(256, 3, down=False)          # 这里配合上面的maxpool1(_d)实现降维
         self.encoder_layer1_d = get_resblock_encoder(256, 3, down=False)
@@ -170,9 +200,8 @@ class ASENet(gluon.nn.HybridBlock):
         self.trans1 = get_resblock_decoder(256, 6)
         self.trans2 = get_resblock_decoder(128, 4)
         self.trans3 = get_resblock_decoder(64, 3)
-        self.trans4 = get_resblock_decoder(64, 3)
-        self.trans5 = get_resblock_decoder(64, 3, up=False)
-
+        self.trans4 = get_resblock_decoder(64, 3, dimskeep=True)
+        self.trans5 = get_resblock_decoder(64, 3, dimskeep=True, up=False)
         self.finalconv = gluon.nn.Conv2DTranspose(channels=class_nums, kernel_size=4, strides=2, padding=1, use_bias=False, weight_initializer=mx.init.Bilinear())
 
         # Agents Part
@@ -189,22 +218,21 @@ class ASENet(gluon.nn.HybridBlock):
         self.conv_side_output4 = get_convlayer(class_nums, ks=1, s=1, bn=False, act=False)
 
     def hybrid_forward(self, F, rgb, depth, *args, **kwargs):
-        # TODO: construct the whole network basing on above calss variables
         # Encoder Part
         conv1 = self.conv1(rgb)
         conv1_d = self.conv1_d(depth)
 
-        layer1 = self.encoder_layer1(self.maxpool1(rgb))
-        layer1_d = self.encoder_layer1_d(self.maxpool1_d(depth))
+        layer1 = self.encoder_layer1(self.maxpool1(conv1 + conv1_d))
+        layer1_d = self.encoder_layer1_d(self.maxpool1_d(conv1_d))
 
-        layer2 = self.encoder_layer2(rgb)
-        layer2_d = self.encoder_layer2_d(depth)
+        layer2 = self.encoder_layer2(layer1 + layer1_d)
+        layer2_d = self.encoder_layer2_d(layer1_d)
 
-        layer3 = self.encoder_layer3(rgb)
-        layer3_d = self.encoder_layer3_d(depth)
+        layer3 = self.encoder_layer3(layer2 + layer2_d)
+        layer3_d = self.encoder_layer3_d(layer2_d)
 
-        layer4 = self.encoder_layer4(rgb)
-        layer4_d = self.encoder_layer4_d(depth)
+        layer4 = self.encoder_layer4(layer3 + layer3_d)
+        layer4_d = self.encoder_layer4_d(layer3_d)
 
         # Agent Part
         agent4 = self.agent4(layer4 + layer4_d)
@@ -218,7 +246,6 @@ class ASENet(gluon.nn.HybridBlock):
         trans2 = self.trans2(agent3 + trans1)
         trans3 = self.trans3(agent2 + trans2)
         trans4 = self.trans4(agent1 + trans3)
-
         finaloutput = self.finalconv(self.trans5(agent0 + trans4))
 
         # Side Output Part
@@ -240,5 +267,9 @@ class target_loss(gluon.loss.Loss):
         pass
 
 
+# 训练的时候，可以通过增加RGB图像的增广来实现模型对Depth的依赖，间接学习RGB与Depth之间的对应关系，如
+#  RGB的随机裁剪以及变形等
+#  RGB的亮度变化，此时可以认为学习到了Depth对应RGB亮度之间的鲁棒性!
 def train_target():
     pass
+
