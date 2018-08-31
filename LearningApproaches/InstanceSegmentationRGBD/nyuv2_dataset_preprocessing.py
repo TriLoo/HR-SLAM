77e0e037b9c43b7fa8cd8291d7ad25fc2e837119
smh <song_mh@yeah.net>
2018-08-31 23:45:11 +0800

    add logging

--- LearningApproaches/InstanceSegmentationRGBD/nyuv2_dataset_preprocessing.py
+++ LearningApproaches/InstanceSegmentationRGBD/nyuv2_dataset_preprocessing.py
@@ -8,6 +8,7 @@ import h5py
 import numpy as np
 import csv
 from scipy.io import loadmat
+import logging     # Ref: python.jobbole.com/81666
 
 
 def map_class_13(label_img, map_40, map_13):
@@ -29,13 +30,19 @@ def map_class_13(label_img, map_40, map_13):
 
 
 def generate_csv_dataset(input_file, train_idx, test_idx, class_map_40, class_map_13, data_file=None, label_file=None):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    logger.info('Starting split the nyuv2 dataset into train & test .csv files.')
     f = h5py.File(input_file)
     images = f['images'][:]     # shape = (1449, 3, 640, 480)
     depths = f['depths'][:]     # shape = (1449, 640, 480)
-    labels = f['labels'][:]     # shape = (1449, 640, 380)
+    labels = f['labels'][:]     # shape = (1449, 640, 480)
     f.close()
+    logger.info('Read images, depths, labels done.')
+    print('data tyep of labels: ', labels.dtype)    # uint16
     labels = map_class_13(labels, class_map_40, class_map_13)
     depths = depths[:, np.newaxis, :, :]
+    logger.info('Recast the labels done.')
     print('shape of images: ', images.shape)
     print('shape of depths: ', depths.shape)
     print('shape of labels: ', labels.shape)
@@ -46,30 +53,43 @@ def generate_csv_dataset(input_file, train_idx, test_idx, class_map_40, class_ma
     test_img = images[test_idx, :, :, :]
     test_dep = depths[test_idx, :, :, :]
     test_label = labels[test_idx, :, :]
+    logger.info('Split train and test done.')
 
-    with open('split_train_datas.csv', 'a') as csvfile_train_img:
-        wra = csv.writer(csvfile_train_img)
-        for img, dep in zip(train_img, train_dep):
-            row = np.concatenate((img.ravel(), dep.ravel()))
-            wra.writerow(row)
+    try:
+        with open('split_train_datas.csv', 'a') as csvfile_train_img:
+            wra = csv.writer(csvfile_train_img)
+            for img, dep in zip(train_img, train_dep):
+                row = np.concatenate((img.ravel(), dep.ravel()))
+                wra.writerow(row)
+    except Exception:
+        logger.error('Failed to store train datas.csv', exc_info=True)
 
-    with open('split_train_labels.csv', 'a') as csvfile_train_label:
-        wrb = csv.writer(csvfile_train_label)
-        for label in train_label:
-            wrb.writerow(label.ravel())
+    try:
+        with open('split_train_labels.csv', 'a') as csvfile_train_label:
+            wrb = csv.writer(csvfile_train_label)
+            for label in train_label:
+                wrb.writerow(label.ravel())
+    except Exception:
+        logger.error('Failed to store train_labels.csv', exc_info=True)
 
-    with open('split_test_datas.csv', 'a') as csvfile_test_img:
-        wrc = csv.writer(csvfile_test_img)
-        for img, dep in zip(test_img, test_dep):
-            row = np.concatenate((img.ravel(), dep.ravel()))
-            wrc.writerow(row)
+    try:
+        with open('split_test_datas.csv', 'a') as csvfile_test_img:
+            wrc = csv.writer(csvfile_test_img)
+            for img, dep in zip(test_img, test_dep):
+                row = np.concatenate((img.ravel(), dep.ravel()))
+                wrc.writerow(row)
+    except Exception:
+        logger.error('Failed to store test_datas.csv', exc_info=True)
 
-    with open('split_test_labels.csv', 'a') as csvfile_test_label:
-        wrd = csv.writer(csvfile_test_label)
-        for label in test_label:
-            wrd.writerow(label.ravel())
+    try:
+        with open('split_test_labels.csv', 'a') as csvfile_test_label:
+            wrd = csv.writer(csvfile_test_label)
+            for label in test_label:
+                wrd.writerow(label.ravel())
+    except Exception:
+        logger.error('Failed to store test_labels.csv', exc_info=True)
 
-    print('Done.')
+    print('All Done.')
 
 
 if __name__ == '__main__':
@@ -95,5 +115,3 @@ if __name__ == '__main__':
     class_map_13 = np.reshape(class_map_13, newshape=-1)
 
     generate_csv_dataset(input_file=file_name, train_idx=train_idx, test_idx=test_idx, class_map_40=class_map_40, class_map_13=class_map_13)
-
-
